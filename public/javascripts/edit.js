04f58a1592fc4447a14f86a56280f706c384ae89
haliluyalei <fulei_master@163.com>
2016-11-20 18:36:05 +0800

    增加js报错后的时间戳  和 alert提示

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,3 +1,9 @@
 setTimeout(function () {
-  throw Error("~!@#$%^&*()_+=-[]{};'\|\":,./?><./  ");
+	//获取毫秒 添加到结尾，得到不同的错误结果
+	var myDate = new Date();
+	myDate.getMilliseconds();
+	var errinfo = "~!@#$%^&*()_+=-[]{};'\|\":,./?><./  ";
+	errinfo+="获取毫秒 添加到结尾，得到不同的错误结果 =>"
+	errinfo+= myDate.getMilliseconds();
+  throw Error(errinfo);
 }, 1);
