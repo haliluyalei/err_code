5610209e0894e8067b1738706c8b67d48a4ff61a
haliluyalei <fulei_master@163.com>
2016-12-02 10:23:00 +0800

    增加路由显示回调并写文件

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var router = require('koa-router')();
-
+var fs = require('fs');
 router.get('/', function *(next) {
   yield this.render('users', {
     title: 'users!'
@@ -14,6 +14,13 @@ router.get('/alert', function *(next) {
   yield this.render('alert', {
     title: 'alert!'
   });
+});
+router.post('/post', function *(next) {
+  console.log(this.request.body);
+  fs.writeFileSync(__dirname + '/_' + new Date().getTime() + '.txt', JSON.stringify(this.request.body, undefined, 2), 'utf-8');
+  this.body = {
+	  ret: 0
+  };
 });
 
 module.exports = router;
